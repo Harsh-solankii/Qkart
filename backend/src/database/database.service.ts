42603170e34b35158d733124935f43834f0cbe44
Harsh-solanki-2024 <harshu.solanki.2024@gmail.com>
2024-10-03 15:47:34 +0530

    db err 3

--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -13,6 +13,7 @@ export const databaseservice = [
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,    
+            dialectModule: require('mysql2')
           });
           sequelize.addModels([user]);
 
