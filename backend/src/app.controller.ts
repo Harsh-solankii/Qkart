917bfeb261561728fe9d7db907ad1d7eee46ef8e
Harsh-solanki-2024 <harshu.solanki.2024@gmail.com>
2024-10-03 17:41:13 +0530

    test route added

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SetMetadata } from '@nestjs/common';
 
-@Controller()
+export const Public = () => SetMetadata('isPublic', true);
+
+@Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  gettest(): string {
+    return 'just testing for deployment and it showing if it worked';
   }
 
   @Post()
